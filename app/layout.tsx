b147091bd75209f6794fbbf522120ef9e727a0af
henry van niekerk <henryvanniekerk@henrys-Mac-mini.local>
2026-01-08 15:44:52 +0200

    Remove global Mixed Media header logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -9,18 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-sans">
-        {/* Header */}
-        <header className="w-full flex justify-center py-6">
-          <Image
-            src="/mm.svg"
-            alt="Mixed Media logo"
-            width={120}
-            height={40}
-            priority
-          />
-        </header>
-
-        {/* Page content */}
+        {/* Page content only */}
         <main>{children}</main>
       </body>
     </html>
